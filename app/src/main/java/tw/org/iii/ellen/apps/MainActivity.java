4ae5e0a407b04171476e016864b050c5be067e49
ellen <ellen60538@gmail.com>
2020-02-23 16:04:30 +0800

    02231604

--- app/src/main/java/tw/org/iii/ellen/apps/MainActivity.java
+++ app/src/main/java/tw/org/iii/ellen/apps/MainActivity.java
@@ -3,12 +3,30 @@ package tw.org.iii.ellen.apps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv1 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.tx1) ;
+    }
+
+    public void click(View view) {
+
+        HashSet<Integer> set = new HashSet<>() ;
+        while (set.size() < 6){
+            set.add((int)(Math.random()*49 + 1)) ;
+        }
+        Log.v("ellen",set.toString()) ;
+        tv1.setText(set.toString()) ;
+
     }
 }
